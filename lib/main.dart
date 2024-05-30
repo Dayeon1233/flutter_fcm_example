41b92ffec886cdb6ac83fee80d4a01041a198208
Chouchou <swdy0393@lgcns.com>
2024-05-30 15:47:46 +0900

    feature:fcm android 알림 완료

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
